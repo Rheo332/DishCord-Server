5ec3486e2c4f787f39add6027446ac187eeefc13
rhe <qbbqbbb@gmail.com>
2025-06-24 20:13:10 +0200

    test fix

--- index.js
+++ index.js
@@ -21,15 +21,10 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "DELETE", "OPTIONS"],
-  })
-);
+app.use(cors());
 
 app.use("/uploads", express.static("public/uploads"));
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json());
 
 app.use(passport.initialize());
 
